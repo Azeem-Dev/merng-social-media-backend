859f215996e5b659a798a72fc939a28a88f57223
Azeem Sharif <72242160+Azeem-Dev@users.noreply.github.com>
2022-01-09 13:17:12 +0500

    resolved db issue

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
+const DB_PASSWORD = "Azeemz511";
 const SECRET_KEY = "SOME VERY SECRET KEY";
 const DB_CONNECTION_STRING = "mongodb://localhost:27017/merng-social-media-app";
-const DB_CONNECTION_STRING_LIVE =
-  "mongodb+srv://azeem:azeem@cluster0.aht6s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const DB_CONNECTION_STRING_LIVE = `mongodb+srv://azeem:${DB_PASSWORD}@cluster0.aht6s.mongodb.net/merng-social-media-app?retryWrites=true&w=majority`;
 module.exports = {
   SECRET_KEY,
   DB_CONNECTION_STRING,
