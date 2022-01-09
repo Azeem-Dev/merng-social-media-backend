b5a77a4648aa23daf15219cd0880164b11372da3
Azeem Sharif <72242160+Azeem-Dev@users.noreply.github.com>
2022-01-09 12:41:04 +0500

    removed config.js from gitignore file

---
+++ config.js
@@ -0,0 +1,9 @@
+const SECRET_KEY = "SOME VERY SECRET KEY";
+const DB_CONNECTION_STRING = "mongodb://localhost:27017/merng-social-media-app";
+const DB_CONNECTION_STRING_LIVE =
+  "mongodb+srv://azeem:azeem@cluster0.aht6s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+module.exports = {
+  SECRET_KEY,
+  DB_CONNECTION_STRING,
+  DB_CONNECTION_STRING_LIVE,
+};
